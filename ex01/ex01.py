51c86909223f56a751ab70e12463fb7dbd49071f
CAD Client <CAD Client>
2016-03-22 13:15:31 +0900

    ex01.py 내용추가

--- ex01/ex01.py
+++ ex01/ex01.py
@@ -1,2 +1,6 @@
-print("Hello World")
-print("Dooho KWAK")
+print "Hello World"
+print "Hello Again"
+print "I like typing this."
+print "Yay! Printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
