79f39425c33725e9574983ecc05ae4c097ebe421
CAD Client <CAD Client>
2016-05-24 09:52:23 +0900

    ex34.py 내용추가

---
+++ ex34/ex34.py
@@ -0,0 +1,12 @@
+# -*-coding:utf8
+
+animals = ['곰','호랑이','펭귄','얼룩말']
+bear = animals[0]
+tiger = animals[1]
+penguin = animals[2]
+zebra = animals[3]
+
+print ("bear=", bear)
+print ("tiger=", tiger)
+print ("penguin=", penguin)
+print ("zebra =", zebra)
